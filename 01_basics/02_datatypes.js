6d47e053e8fe7046127f8a89c2a02bf759ac62d6
muditGupta1310 <techskill2007@gmail.com>
2024-12-05 06:54:57 +0000

    container config done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all Js code as newer version
+
+// alert("Hello")  we are using nodejs not browser thats why the alert will throw an error
+
+let name= "mudit"
+let age = 23
+let loggedIn = true
+let state;
+
+// number range (2^53)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
+
