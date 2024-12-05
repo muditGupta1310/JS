b0256986d34d8cf9d0a9b60fdf8e95709b6b76cd
muditGupta1310 <techskill2007@gmail.com>
2024-12-05 07:07:41 +0000

    container config done

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "3abx"
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33ac" => NaN but type comes out be true 
+// wich is quite confusing so keep this in mind
+// true => 1; false=>0
+// null => 0
+// undefined = NaN but types again comes out be Number
+
+
+let isLogged = "mudit"
+
+let booleanIsLoggedIn = Boolean(isLogged)
+console.log(booleanIsLoggedIn);
+
+
+// conversion to Boolean 
+// 1 => true; 0=> false
+// "" => false
+// "mudit" = true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof(stringNumber));
