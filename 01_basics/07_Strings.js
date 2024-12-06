a3e45a73bac2182af42a5fc8f4d29d5eb930bbbd
muditGupta1310 <techskill2007@gmail.com>
2024-12-06 07:14:18 +0000

    container config done

---
+++ 01_basics/07_Strings.js
@@ -0,0 +1,45 @@
+ const name = "Mudit"
+ const repo = 50
+
+//  console.log(`Hello my name is ${name} and my repo count is ${repo}`);
+
+
+ const gameName = new String('mudit_13_com')
+//  console.log(gameName);
+
+// console.log(typeof(gameName))
+
+
+// console.log(gameName[0])
+
+
+// console.log(gameName.__proto__);
+
+
+// console.log(gameName.toUpperCase());
+
+// console.log(gameName.charAt(2));
+
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-4,-1)
+// console.log(anotherString);
+
+
+// const nS1 = "      mudit       "
+// console.log(nS1);
+// console.log(nS1.trim());
+
+
+
+// const url = "https://mudit%20gupta.com"
+
+// console.log(url.replace('%20','-'))
+
+
+// console.log(url.includes('mudit'))
+
+console.log(gameName.at())
