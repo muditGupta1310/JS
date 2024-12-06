7287bab12486184b1e80acb39716d90332a38fd8
muditGupta1310 <techskill2007@gmail.com>
2024-12-06 09:31:18 +0000

    container config done

---
+++ 01_basics/09_date_time.js
@@ -0,0 +1,58 @@
+// Date
+
+let myDate = new Date()
+// const options = { timeZone: 'Asia/Kolkata', hour12: true, timeStyle: 'medium', dateStyle: 'medium' };
+// console.log(new Intl.DateTimeFormat('en-IN', options).format(myDate));
+
+
+// console.log(myDate.toString());
+
+// console.log(myDate.toDateString());
+
+// console.log(myDate.toISOString());
+
+// console.log(myDate.toJSON());
+
+// console.log(myDate.toLocaleString());
+
+// console.log(myDate.toLocaleDateString());
+
+// console.log(myDate.toLocaleTimeString());
+
+// let myCreatedDate = new Date(2023,0,20,5,3)
+
+// let myCreatedDate = new Date("01-14-2023"))
+
+
+let myCreatedDate = new Date("2023-01-14")
+
+
+// console.log(myCreatedDate.toDateString());
+
+let myTimeStamp = Date.now()
+
+// console.log(Math.floor(myTimeStamp/1000));
+
+// console.log(myCreatedDate.getTime())
+
+
+let newDate =new Date()
+
+// console.log(newDate);
+
+// console.log(newDate.getDate());
+
+// console.log(newDate.getMonth()+1);
+
+// console.log(newDate.getFullYear());
+
+// console.log(newDate.getHours())
+
+
+// newDate.toLocaleDateString('Asia/Kolkata',{weekday:"long"})  // Error in this
+console.log(newDate)
+
+
+
+
+
