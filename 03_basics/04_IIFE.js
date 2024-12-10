f94bcc060347a79c9e54405059e876a68409fd40
muditGupta1310 <techskill2007@gmail.com>
2024-12-10 02:32:51 +0000

    container config done

--- 03_basics/04_IIFE.js
+++ 03_basics/04_IIFE.js
@@ -1,1 +1,12 @@
-// Immediately Invoked Function Expression (IIFE )
+// Immediately Invoked Function Expression (IIFE)
+
+(function chai(){
+    //named IIFE
+    console.log("DB CONNECTED")
+})();
+
+
+( (name) => {
+    // simple IIFE
+    console.log(`DB CONNECTED TWO ${name}`)
+})("mudit");
