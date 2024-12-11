067a5f9e54efe9044cf40b447be95959bd8b3f76
muditGupta1310 <techskill2007@gmail.com>
2024-12-11 12:43:12 +0000

    container config done

---
+++ 04_control_flow/truth_flasy.js
@@ -0,0 +1,16 @@
+const userEmail = "mudit@ai"
+if(userEmail){
+    console.log("Got user email")
+}
+else{
+    console.log("Don't have user email")
+}
+
+// falsy values
+
+// false,0,-0, BigInt '0n',"",  null, undefined, NaN
+
+
+// truthy values 
+
+// "0", 'false', " ", [], {}, function(){}
