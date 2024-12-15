9257abeb21062d83968c9cc3da24ecd8f07ab3d9
muditGupta1310 <techskill2007@gmail.com>
2024-12-15 02:17:02 +0000

    container config done

--- 04_control_flow/truth_flasy.js
+++ 04_control_flow/truth_flasy.js
@@ -35,9 +35,17 @@ const emptyObj = {}
 
 // Nullish Coalescing Operator (??): null undefined
 
-let val1; 
+// let val1; 
 
-val1 = null ?? 10
+// val1 = null ?? 10
+
+// console.log(val1);
 
 
-console.log(val1);
+// Ternary Operator
+
+// condition? true:false
+
+const iceTea = 100
+iceTea <=80 ? console.log("less than 80") : console.log("more than 80");
+
