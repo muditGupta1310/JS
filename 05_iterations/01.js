39a7ace14eb2df45d0eb04380e33e68909f9b1be
muditGupta1310 <techskill2007@gmail.com>
2024-12-15 18:02:22 +0000

    container config done

--- 05_iterations/01.js
+++ 05_iterations/01.js
@@ -1,13 +1,33 @@
 // for loop
 
-for (let i = 0; i < 10; i++) {
-    console.log(`Outer loop value : ${i}`);
-    for (let j = 0; j < 10; j++) {
-        console.log(`Inner Value : ${j}`);
+// for (let i = 0; i < 10; i++) {
+//     console.log(`Outer loop value : ${i}`);
+//     for (let j = 0; j < 10; j++) {
+//         console.log(`Inner Value : ${j}`);
           
-    }
+//     }
 
-}
+// }
 
 // break and continue
 
+// for (let index = 1; index <= 20; index++) {
+
+//     if (index==5){
+//         console.log("Detected 5")
+//         break;
+//     }
+//     console.log(`Value of i is ${index}`)
+    
+// }
+
+
+for (let index = 1; index <= 10; index++) {
+
+    if (index==5){
+        console.log("Detected 5")
+        continue;
+    }
+    console.log(`Value of i is ${index}`)
+    
+}
