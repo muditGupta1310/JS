935a03bf9412ea01e661aae737c6b31d5e8c28e6
muditGupta1310 <techskill2007@gmail.com>
2024-12-15 18:29:12 +0000

    container config done

---
+++ 05_iterations/for of loop.js
@@ -0,0 +1,75 @@
+// for of 
+
+
+const arr = [1,2,3,4,5]
+
+// for (const ele of arr) {
+//     console.log(ele);
+// }
+
+// const greetings = "Hello World"
+
+// for (const ele of greetings) {
+//     console.log(ele);
+// }
+
+// Maps
+
+const map = new Map()
+
+map.set('IN',"india")
+
+map.set('US',"United States")
+
+map.set('AUS',"Austrailia")
+
+// console.log(map)
+
+// for (const [key,value] of map) {
+//     console.log(key, ':-',value);
+    
+// }
+
+// const myObj = {
+//     'game1':"NFS",
+//     "game2":"Spiderman"
+// }
+
+
+// for (const [key,value] of myObj) {
+//     console.log(key, ":-",value);
+// }
+
+// const myObj2 = {
+//     js: 'javascript',
+//     cpp:"C++",
+//     rb:"ruby",
+//     swift : "swift for apple"
+// }
+
+// for (const key in myObj2) {
+//     console.log(`${key} shortcut is for ${myObj2[key]}`);
+    
+// }
+
+
+// const programming = ['js','rb','py','java','cpp']
+
+// for (const key in programming) {
+//     console.log(`Value for ${key} in programming is ${programming[key]}`);
+    
+// }
+
+
+// const map = new Map()
+
+// map.set('IN',"india")
+
+// map.set('US',"United States")
+
+// map.set('AUS',"Austrailia")
+
+// for (const key in map) {
+//     console.log(key);
+    
+// }
