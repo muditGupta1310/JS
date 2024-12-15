ba056880cdc864e981a70eaaaf4889bee181e4a2
muditGupta1310 <techskill2007@gmail.com>
2024-12-15 17:56:18 +0000

    container config done

---
+++ 05_iterations/01.js
@@ -0,0 +1,13 @@
+// for loop
+
+for (let i = 0; i < 10; i++) {
+    console.log(`Outer loop value : ${i}`);
+    for (let j = 0; j < 10; j++) {
+        console.log(`Inner Value : ${j}`);
+          
+    }
+
+}
+
+// break and continue
+
