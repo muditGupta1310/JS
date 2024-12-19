92c725302ccb96d3aaabd04f4c7569c921b05856
muditGupta1310 <techskill2007@gmail.com>
2024-12-19 07:27:21 +0000

    contianer config done

--- 05_iterations/6.js
+++ 05_iterations/6.js
@@ -1,27 +1,27 @@
-const coding = ["js","ruby","java","python"]
+// const coding = ["js","ruby","java","python"]
 
 
-const values = coding.forEach( (item) => {
-    // console.log(item);   
-    return item
-})
-console.log(values);
+// const values = coding.forEach( (item) => {
+//     // console.log(item);   
+//     return item
+// })
+// console.log(values);     //for each values return nhi krta agar values chahiye then use push command for new array
 
 
-// const myNums = [1,2,3,4,5,6,7,8,9,10]
+const myNums = [1,2,3,4,5,6,7,8,9,10]
 
 // const newNums = myNums.filter( (num) => {
 //     return num > 4
 // })
 
-// const newNums = []
+const newNums = []
 
-// myNums.forEach((num) => {
-//     if(num > 4) {
-//         newNums.push(num)
-//     }
-// })
+myNums.forEach((num) => {
+    if(num > 4) {
+        newNums.push(num)
+    }
+})
 
-// console.log(newNums);
+console.log(newNums);
 
 
