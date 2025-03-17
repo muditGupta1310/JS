c1a3e39064b46076df2f63f5aec621ddce973aba
muditGupta1310 <techskill2007@gmail.com>
2025-03-17 05:15:10 +0000

    container .config done

--- 08_oops/oops.js
+++ 08_oops/oops.js
@@ -4,12 +4,50 @@ const user = {
     signedIn : true,
 
     getUserDetails : function () {
-        console.log("Got user Details from database");
+        // console.log("Got user Details from database");
+        // console.log(`Username : ${this.username}`);
+        console.log(this);
         
     }
 
 }
 
-console.log(user.username);
+const user2 = {
+    username: "mudit",
+    loginCount : 7,
+    signedIn : true,
 
-console.log(user.getUserDetails());
+    getUserDetails : function () {
+        // console.log("Got user Details from database");
+        // console.log(`Username : ${this.username}`);
+        console.log(this);  
+    }
+}
+
+
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this . isLoggedIn = isLoggedIn
+
+    // return this
+}
+
+
+const userOne = new User("mudit", 4, true)
+
+const UserTwo =  new User("MuditGupta", 5, false)
+
+console.log(userOne);
+
+console.log(UserTwo);
+
+
+// new creates an empty object
+// 2nd, Constructor Function is called by new Keywords
+// 3rd, values injected 
+// 4th return the values
+
+
