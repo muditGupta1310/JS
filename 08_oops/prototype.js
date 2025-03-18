24ab9c806703e39c7bda89a8c4aefe50758ba7fe
muditGupta1310 <techskill2007@gmail.com>
2025-03-18 06:48:25 +0000

    container .config done

--- 08_oops/prototype.js
+++ 08_oops/prototype.js
@@ -18,4 +18,49 @@ Object.prototype.mudit = function(){
     console.log(`Mudit is present in all objects`);
 }
 
-heroPower.mudit()
+// heroPower.mudit()
+
+// myHeros.mudit()
+
+Array.prototype.heymudit = function(){
+    console.log(`Mudit says hello`);
+    
+}
+
+
+// myHeros.heymudit()
+
+// heroPower.heymudit()
+
+
+const user = {
+    name : 'xyz',
+    email : 'xyz@gmail.com'
+}
+
+const Teacher = {
+    makeVideo : true
+}
+
+const TeachingSupport = {
+    isAvailable : false
+}
+
+const TASupport = {
+    makeAssignment : 'JS assignment',
+    fullTime : true,
+    __proto__ : TeachingSupport
+}
+
+// Teacher.__proto__ = user
+
+
+let anotherUsername = "qwertyuiop"
+
+String.prototype.truelength = function(){
+    console.log(`${this}`);
+    console.log(`True Length is : ${this.trim().length}`);
+}
+
+anotherUsername.truelength()
+
