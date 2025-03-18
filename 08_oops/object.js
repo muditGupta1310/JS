359fca39c254be685f41377c328746839971ea91
muditGupta1310 <techskill2007@gmail.com>
2025-03-18 06:09:08 +0000

    container .config done

--- 08_oops/object.js
+++ 08_oops/object.js
@@ -9,3 +9,24 @@ console.log(multipleBy5.power);
 console.log(multipleBy5.prototype);
 
 
+function createUser(username, score){
+    this.username = username
+    this.score = score
+}
+
+
+createUser.prototype.increment = function(){
+    this.score++
+}
+
+createUser.prototype.printme = function(){
+    console.log(`price is ${this.score}`);
+    
+}
+
+const u1 = new createUser("u1", 25)
+
+const u2 = new createUser("u2", 12)
+
+
+u1.printme()
