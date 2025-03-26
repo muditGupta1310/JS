d50c4503b8650faa4ced91d79d706a762dedad0f
muditGupta1310 <techskill2007@gmail.com>
2025-03-26 20:27:14 +0000

    container .config done

---
+++ 08_oops/staticprop.js
@@ -0,0 +1,32 @@
+class User {
+    constructor(username){
+        this.username = username
+        }
+
+    logMe(){
+        console.log(`Username : ${this.username}`);
+        
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+const mudit = new User("mudit")
+// console.log(mudit.createId());
+
+class Teacher extends User{
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+
+const iphone = new Teacher("iphone", "i@phone.com")
+
+iphone.logMe()
+
+
+console.log(iphone.createId());
